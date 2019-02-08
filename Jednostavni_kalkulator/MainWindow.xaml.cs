739fa67cffa8302c36f154d989c297595fe58b9d
Jakov Delas <jakov.delas@ericsson.com>
2019-02-08 16:10:50 +0100

    Add project files.

---
+++ Jednostavni_kalkulator/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jednostavni_kalkulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Parse(string data)
+        {
+            char[] delimiters = new char[] { '+', '-', '*', '/' };
+            string[] parts = data.Split(delimiters);
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button tipka = (Button)sender;
+            lblMain.Content = (string)lblMain.Content + "" + (string)tipka.Content;
+            Parse((string)lblMain.Content);
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+           // Button tipka = (Button)sender;
+            lblMain.Content =  " = ";
+
+        }
+
+        private void Button_Click_Zbrajanje(object sender, RoutedEventArgs e)
+        {
+            // Button tipka = (Button)sender;
+            lblMain.Content = " + ";
+
+        }
+        private void Button_Click_Oduzimanje(object sender, RoutedEventArgs e)
+        {
+            // Button tipka = (Button)sender;
+            lblMain.Content = " - ";
+
+        }
+
+        private void Button_Click_Mnozenje(object sender, RoutedEventArgs e)
+        {
+            // Button tipka = (Button)sender;
+            lblMain.Content = " * ";
+
+        }
+
+        private void Button_Click_Dijeljenje(object sender, RoutedEventArgs e)
+        {
+            // Button tipka = (Button)sender;
+            lblMain.Content = " / ";
+
+        }
+
+        private void Button_Click_Clear(object sender, RoutedEventArgs e)
+        {
+            // Button tipka = (Button)sender;
+            lblMain.Content = " ";
+
+        }
+
+
+    }
+}
